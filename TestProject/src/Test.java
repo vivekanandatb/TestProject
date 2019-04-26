5aac455c78cff78149e6d6e697fdc310e45836c8
vivekanandatb <vivek.g1691@gmail.com>
2019-04-26 12:46:18 +0530

    Change the first element name in array

--- TestProject/src/Test.java
+++ TestProject/src/Test.java
@@ -3,6 +3,7 @@ public class Test {
 	
 	 public static void main(String[] args) {
 		    String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+		    cars[0] = "Lambergini";
 		    System.out.println(cars[0]);
 		  }
 }
