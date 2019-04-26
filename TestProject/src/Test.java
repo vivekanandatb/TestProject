78fcec57144a8dfa71b042319f9f975d14f77117
vivekanandatb <vivek.g1691@gmail.com>
2019-04-26 12:49:50 +0530

    Length of array

--- TestProject/src/Test.java
+++ TestProject/src/Test.java
@@ -3,7 +3,6 @@ public class Test {
 	
 	 public static void main(String[] args) {
 		    String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
-		    cars[0] = "Lambergini";
-		    System.out.println(cars[0]);
+		    System.out.println(cars.length);
 		  }
 }
